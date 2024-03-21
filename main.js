8dc6468ef10e1ee2c45dd6afea26d2eef518921c
Joko109dogbn <105368445+Joko109dogbn@users.noreply.github.com>
2024-03-21 08:23:35 -0300

    ok

--- main.js
+++ main.js
@@ -7,10 +7,8 @@ for(let i=0; i <botoes.length; i++){
         for(let j=0;botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-
         }        
         botoes[j].classList.add("ativo");
         textos[j].classList.add("ativo");
-
     }
 }
